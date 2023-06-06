19d9ac563947e98da43404452a8cb1a25cbdc4e6
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-06 10:43:41 -0300

    up

---
+++ ex01.py
@@ -0,0 +1,18 @@
+#
+#
+# autores: Michel
+#
+# data: 06-06-2023
+
+# 1.	Faça um programa que leia um número inteiro e verifique se ele é par ou ímpar.
+
+# Entrada de dados
+num = int(input("Digite um número inteiro: "))
+# Processamento
+if num % 2 == 0:
+    print("O número é par")
+else:
+    print("O número é ímpar")
+    
+# Saída de dados
+print("Fim do programa")
