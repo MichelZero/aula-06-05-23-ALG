6c7513731a9504828b3fc1a056fd98a055d4c30f
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-06 11:00:04 -0300

    up

---
+++ ex03.py
@@ -0,0 +1,16 @@
+#
+#
+# autores: Michel
+#
+# data: 06-06-2023
+
+# 3. Escreva um programa em Python que imprima todos os números de 1 a 50 que são múltiplos 
+# de 7. Utilize um loop for e a estrutura IF.
+
+# Entrada de dados
+# Processamento
+for i in range(1, 51):
+    if i % 7 == 0:
+        print(i)
+# Saída de dados
+print("Fim do programa")
