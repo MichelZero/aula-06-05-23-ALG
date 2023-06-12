c1ab24b2a689b4d1a0688ae32e12e5205554b9a7
Michel <106674127+MichelZero@users.noreply.github.com>
2023-06-12 09:03:25 -0300

    up

--- ex15.py
+++ ex15.py
@@ -13,8 +13,11 @@
 # Entrada de dados
 num = int(input("Digite um número inteiro positivo: "))
 # Processamento
-for i in range(1, num + 1):
-    if i % 2 != 0:
-        print(i)
+if num >= 1:  
+  for i in range(1, num + 1):
+      if i % 2 != 0:
+          print(i)
+else:
+  print(f"{num} não é positivo!")
 # Saída de dados
 print("Fim do programa")
